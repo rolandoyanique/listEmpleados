7dbb807596600ae12e3093c8364e21637aff77c7
rolyan66@gmail.com <rolyan66@gmail.com>
2025-09-01 11:41:05 -0400

    Se realizo el creado del componente y el componente hijo

---
+++ src/app/components/empleado-list/empleado-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado-list',
+  templateUrl: './empleado-list.component.html',
+  styleUrls: ['./empleado-list.component.css']
+})
+export class EmpleadoListComponent {
+
+}
