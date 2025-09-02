6d462349274319434545efd2033ecc46da061c6f
rolyan66@gmail.com <rolyan66@gmail.com>
2025-09-02 11:20:32 -0400

    Se realizo la adicion de la tabla y los radio buttons para empleados ademas del array de empleados

--- src/app/components/empleado-list/empleado-list.component.ts
+++ src/app/components/empleado-list/empleado-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from 'src/app/models/Empleado';
 
 @Component({
   selector: 'app-empleado-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./empleado-list.component.css']
 })
 export class EmpleadoListComponent {
-
+  listEmpleados:Empleado[] = [
+    {legajo: 1, nombre: "Juan", apellido: "Perez", sexo: "Masculino", salario: 150000},
+    {legajo: 2, nombre: "Ana", apellido: "Gomez", sexo: "Femenino", salario: 200000},
+    {legajo: 3, nombre: "Carla", apellido: "Lopez", sexo: "Femenino", salario: 250000},
+    {legajo: 4, nombre: "Luis", apellido: "Martinez", sexo: "Masculino", salario: 300000},
+    {legajo: 5, nombre: "Marta", apellido: "Rodriguez", sexo: "Femenino", salario: 350000}
+  ];
 }
